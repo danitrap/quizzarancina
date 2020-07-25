79bcbdc9a975f2fd47255d7dd48f4968cec06db9
Daniele Trapani <daniele.trapani@outlook.it>
2020-07-25 17:44:09 +0200

    fix: silence react dependency dumb error

--- src/composables/useSurvey.ts
+++ src/composables/useSurvey.ts
@@ -97,6 +97,7 @@ export function useSurvey({ results, questions }: ISurvey) {
   const reset = () =>
     dispatch({ type: SurveyActionEnum.RESET, payload: questions });
 
+  // eslint-disable-next-line
   useEffect(() => reset(), [questions]);
 
   return {
